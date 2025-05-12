7e627d8196c879061dc863a05f15a7c2e409eef5
Shay <iwishiwuzskiing@gmail.com>
2025-05-11 17:27:39 -0700

    added test script that exercises api

---
+++ test.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+import asyncio
+from config import username, password
+from pylitterbot import Account
+
+# Set email and password for initial authentication.
+#username = "Your username"
+#password = "Your password"
+
+
+async def main():
+    # Create an account.
+    account = Account()
+
+    try:
+        # Connect to the API and load robots.
+        print(f"Connecting to account for {username}...")
+        await account.connect(username=username, password=password, load_robots=True)
+
+        # Print robots associated with account.
+        print("Robots:")
+        for robot in account.robots:
+            print(robot)
+    except:
+        print(f"Failed to connect to account for {username}...")
+    finally:
+        # Disconnect from the API.
+        await account.disconnect()
+
+
+if __name__ == "__main__":
+    print(f"username: {username}\npassword: {password}")
+    asyncio.run(main())
